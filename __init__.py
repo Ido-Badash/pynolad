15e07781505dd324288084f63d4c48211e34f2e8
Ido-Badash <idoba12012011@gmail.com>
2025-06-10 16:06:55 +0300

    Bump version to 0.1.7 and clean up __all__ declaration in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,7 @@
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 from .animated_game_object import AnimatedGameObject
 from .camera import Camera
 from .game_object import GameObject
+from .pynolad_plus import *
 from .worlds import ClosedWorld, OpenWorld
-
-__all__ = [
-    "Camera",
-    "GameObject",
-    "AnimatedGameObject",
-    "ClosedWorld",
-    "OpenWorld",
-    "Scene",
-    "SceneManager",
-]

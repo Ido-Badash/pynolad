aa4e5da73b2cd23aa9580d3c2e2c5a264916cb54
Ido-Badash <idoba12012011@gmail.com>
2025-06-13 01:20:58 +0300

    Add GravitedClosedWorld to module exports in __init__.py

--- worlds/__init__.py
+++ worlds/__init__.py
@@ -1,5 +1,6 @@
 from .closed_world import ClosedWorld
+from .gravited_closed_world import GravitedClosedWorld
 from .open_world import OpenWorld
 from .world import World
 
-__all__ = ["World", "OpenWorld", "ClosedWorld"]
+__all__ = ["World", "OpenWorld", "ClosedWorld", "GravitedClosedWorld"]
